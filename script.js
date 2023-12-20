34fd8b62df33d8cf46cb16b48957911f1d957572
Cengiz <154424137+cengizcinar01@users.noreply.github.com>
2023-12-20 20:41:29 +0100

    Implement Sprite Rendering in Animation Function
    Revised the animate function to include sprite rendering. Replaced the basic rectangle drawing with ctx.drawImage, using the 'shadow_dog.png' image. This update involves drawing a specific portion of the sprite sheet, defined by spriteWidth and spriteHeight, to render the player character. This change marks a significant step towards more complex sprite-based animations.

--- script.js
+++ script.js
@@ -5,10 +5,12 @@ const CANVAS_HEIGHT = (canvas.height = 600);
 
 const playerImage = new Image();
 playerImage.src = 'shadow_dog.png';
+const spriteWidth = 575;
+const spriteHeight = 523;
 
 function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    ctx.fillRect(50, 50, 100, 100);
+    ctx.drawImage(playerImage, 0 * spriteWidth, 0 * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
     requestAnimationFrame(animate);
 }
 animate();
