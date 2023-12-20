d6c82b674dd6b0c52aa5baa87914ef888afbb1f9
Cengiz <154424137+cengizcinar01@users.noreply.github.com>
2023-12-20 20:58:21 +0100

    Refine Frame Calculation in Animation Loop
    Improved the sprite animation logic by refining the frame calculation method. Now, 'frameX' is calculated using a combination of 'gameFrame', 'staggerFrames', and the modulo operator. This update ensures a more precise and efficient frame selection for smoother animation transitions. The new calculation method provides a clearer structure for the animation sequence.

--- script.js
+++ script.js
@@ -14,11 +14,9 @@ const staggerFrames = 5;
 
 function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    ctx.drawImage(playerImage, frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
-    if (gameFrame % staggerFrames == 0) {
-        if (frameX < 6) frameX++;
-        else frameX = 0;
-    }
+    let position = Math.floor(gameFrame / staggerFrames) % 6;
+    frameX = spriteWidth * position;
+    ctx.drawImage(playerImage, frameX, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
 
     gameFrame++;
     requestAnimationFrame(animate);
