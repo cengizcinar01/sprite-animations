3d9600e1e953b72b453001d0097cc84538fb3ce1
Cengiz <154424137+cengizcinar01@users.noreply.github.com>
2023-12-20 20:50:36 +0100

    Implement Animation Staggering Mechanism
    Updated the animation function to include a staggered frame mechanism. Introduced 'gameFrame' and 'staggerFrames' variables to control the animation speed. Now, the sprite's frame updates occur every few frames, based on 'staggerFrames', leading to a smoother animation sequence. This enhancement makes the animation more visually appealing and better paced.

--- script.js
+++ script.js
@@ -9,12 +9,18 @@ const spriteWidth = 575;
 const spriteHeight = 523;
 let frameX = 0;
 let frameY = 0;
+let gameFrame = 0;
+const staggerFrames = 5;
 
 function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     ctx.drawImage(playerImage, frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
-    if (frameX < 6) frameX++;
-    else frameX = 0;
+    if (gameFrame % staggerFrames == 0) {
+        if (frameX < 6) frameX++;
+        else frameX = 0;
+    }
+
+    gameFrame++;
     requestAnimationFrame(animate);
 }
 animate();
